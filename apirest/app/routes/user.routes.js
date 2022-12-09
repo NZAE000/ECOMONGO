dd2aaceb0ad1b86c7a46df27fc3cdceae55802a0
Eliezer Zúñiga <eliezer.zuniga@alumnos.uv.cl>
2022-12-09 20:43:52 -0300

    back: any user routes modified and user controller filter on findAll fixed

--- apirest/app/routes/user.routes.js
+++ apirest/app/routes/user.routes.js
@@ -7,7 +7,7 @@ router.route("/add").post(create);
 router.route("/all").get(findAll);
 router.route("/:rut").get(findId);
 router.route("/:rut").put(update);
-router.route("/:id").delete(deleteOne);
+router.route("/:rut").delete(deleteOne);
 router.route("/").delete(deleteAll);
 
 export default router;
