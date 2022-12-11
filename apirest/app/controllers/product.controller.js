27580238c2388bc7392118dd366c0babb233aa2d
Eliezer Zúñiga <eliezer.zuniga@alumnos.uv.cl>
2022-12-11 20:53:48 -0300

    back: product controller fixed.

--- apirest/app/controllers/product.controller.js
+++ apirest/app/controllers/product.controller.js
@@ -1,122 +1,164 @@
 import ProductModel from "../models/product.model.js"
 
 
-const create = async ( req, res ) => 
+const create = async(req, res) => 
 {
     try {
         if (!req.body.category && !req.body.nameProduct && !req.body.mark && !req.body.price && !req.body.stock && !req.body.idProvider)
             return res.status(400).send({ message: "Content product can not be empty!" });
 
         const newProduct = new ProductModel(req.body);
-
         await newProduct.save();
 
         return res.status(200).json({
             success: true,
-            message: 'producto creado',
+            message: 'Product created',
         });
         
     } catch ( err ){
-        return res.status(500).json({message: err.message});
+        return res.status(500).json({ message: err.message });
     };
 };
 
-const findAll = async ( req, res ) => 
+//asignar condicion de acuerdo al filtro por parámetro
+const filter = (req) =>
+{
+    const {name, ctgry, price} = req.query;
+
+    if (ctgry && price && name) return { $and: [{ category: { $regex: `${ctgry}`, $options: 'i' }}, { price: { $lte: price }}, { nameProduct: { $regex: `${name}`, $options: 'i'}}] };
+    else if (ctgry && price)    return { $and: [{ category: { $regex: `${ctgry}`, $options: 'i' }}, { price: { $lte: price }}] };
+    else if (name && price)     return { $and: [{ nameProduct: { $regex: `${name}`, $options: 'i'}}, { price: { $lte: price }}] };
+    else if (ctgry && name)     return { $and: [{ category: { $regex: `${ctgry}`, $options: 'i'} }, { nameProduct: { $regex: `${name}`, $options: 'i'}}] };
+    else
+        return (ctgry || price || name)? { $or: [{ category: { $regex: `${ctgry}`, $options: 'i'}}, { price: { $lte: price }}, { nameProduct: { $regex: `${name}`, $options: 'i'}}] } : null;
+}
+
+const findAll = async(req, res) => 
 {
     try {
+        const condition = filter(req);
+        const type      = (req.query.typeUser)? req.query.typeUser.toLowerCase() : "u";
 
-        const products = await ProductModel.find().exec();
+        const products = await ProductModel.find(condition, (type=="a")? { __v:0 } : { __v:0, _id: 0, idProvider: 0, createdAt: 0, updatedAt: 0 }).exec();
         return res.status(200).json({
             success: true,
             products: products,
         });
-    }catch ( error ){
-        return error;
+
+    } catch ( err ){
+        return res.status(500).json({ message: err.message });
     };
 };
 
-const findId = async ( req, res ) => {
+const findAllByStock = async(req, res) =>
+{
     try {
+        const stock = req.params.stock;
+        if (stock<0) return res.send({ message: "Incorrect stock value" });
 
-        console.log("ID: ",req.params.id);
-        const foundNew = await ProductModel.findById( req.params.id, '-createdAt -updatedAt -__v' ).exec();
+        const products = await ProductModel.find({ stock: stock }, '-__v -_id');  
+
+        return res.status(200).json({
+            success: true,
+            product: products,
+        });
+
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+}
+
+const findAllFromProvider = async(req, res) =>
+{
+    try {
+        const id_prov  = req.params.id_prov;
+        const products = await ProductModel.find({ idProvider: id_prov }, '-__v -_id');
+
+        return res.status(200).json({
+            success: true,
+            product: products,
+        });
         
-        if(!foundNew){
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+}
+
+const findId = async(req, res) => 
+{
+    try {
+        const foundNew = await ProductModel.findById( req.params.id_prod, '-createdAt -updatedAt -__v' ).exec();
+        
+        if(!foundNew)
             return res.status(404).json({
                 success: false,
-                error: "producto no encontrado"
+                error: "Product not found"
             });
-        };
         
         return res.status(200).json({
             success: true,
             product: foundNew,
         });
 
-
-    }catch ( error ){
-        console.log(error);
-        return error;
+    } catch ( err ){
+        return res.status(500).json({ message: err.message });
     };
 };
 
-const update = async ( req, res ) => {
+const update = async(req, res) => 
+{
     try {
-
         const attributes = req.body;  
        
-        if(!Object.entries(attributes).length){
+        if(!Object.entries(attributes).length)
             return res.status(404).json({
                 success: false,
-                error: "Producto necesita algun parametro para actualizar"
+                error: "Product needs some parameter to update"
             });
-        }
-        await ProductModel.findByIdAndUpdate(req.params.id, attributes).exec();
+    
+        await ProductModel.findByIdAndUpdate(req.params.id_prod, attributes).exec();
 
         return res.status(200).json({
             success: true,
-            message: 'Producto actualizado',
+            message: 'Product updated',
         });
-    }catch ( error ){
-        console.log(error)
-        return error ;
+
+    } catch ( err ){
+        return res.status(500).json({ message: err.message });
     };
 };
 
-const deleteOne = async ( req, res ) => {
+const deleteOne = async(req, res) => 
+{
     try {
+        const productDeleted = await ProductModel.findByIdAndDelete(req.params.id_prod).exec();
 
-        console.log("ID: ",req.params.id);
-        const productDeleted = await ProductModel.findByIdAndDelete(req.params.id).exec();
-        if(!productDeleted){
-            return res.status(404).json({
-                success: false,
-                error: "Producto no encontrado para eliminar"
-            });
-        };
-       
-        return res.status(200).json({
+        return (!productDeleted)? res.status(404).json({
+            success: false,
+            error: "Product not found to delete"
+        }) 
+        : res.status(200).json({
             success: true,
-            message: 'Producto eliminado',
+            message: 'Product removed',
         });
-    }catch ( error ){
-        console.log(error)
-        return  error ;
 
+    } catch ( err ){
+        return res.status(500).json({ message: err.message });
     };
 };
 
-const deleteAll = async ( req, res ) => {
+const deleteAll = async(req, res) => 
+{
     try {
-
         const products = await ProductModel.deleteMany().exec();
         return res.status(200).json({
             success: true,
             products: products,
         });
-    }catch ( error ){
-        return error;
 
+    } catch ( err ){
+        return res.status(500).json({ message: err.message });
     };
 };
-export { create, findAll, findId, update, deleteOne, deleteAll };
+
+export { create, findAll, findAllByStock, findAllFromProvider, findId, update, deleteOne, deleteAll };
--- apirest/app/routes/product.routes.js
+++ apirest/app/routes/product.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { create, findAll, findId, update, deleteOne, deleteAll } from "../controllers/product.controller.js"
+import { create, findAll, findAllByStock, findAllFromProvider, findId, update, deleteOne, deleteAll } from "../controllers/product.controller.js"
 
 const router = Router();
 
 router.route("/add").post(create);
 router.route("/all").get(findAll);
+router.route("/all-byStock/:stock").get(findAllByStock);
+router.route("/all-fromProv/:id_prov").get(findAllFromProvider);
 router.route("/:id_prod").get(findId);
 router.route("/:id_prod").put(update);
 router.route("/:id_prod").delete(deleteOne);
